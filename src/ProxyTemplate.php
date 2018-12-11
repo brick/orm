0e557f76f070eb4f095cd99b9401a5eef8392da3
Benjamin Morel <benjamin.morel@gmail.com>
2018-12-11 22:38:55 +0100

    Use strict types

--- src/ProxyTemplate.php
+++ src/ProxyTemplate.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PROXY_NAMESPACE;
 
 use Brick\ORM\Gateway;
--- src/RepositoryTemplate.php
+++ src/RepositoryTemplate.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace REPO_NAMESPACE;
 
 use Brick\ORM\Gateway;
